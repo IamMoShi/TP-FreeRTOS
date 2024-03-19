23276dbc990d09d4240a77b4648e45f1fb16a63a
Leo F <leofornoff02@gmail.com>
2024-03-19 11:23:52 +0100

    creation du projet pour l'esp sans oled

---
+++ tp_freeRTOS/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "sdkconfig.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "freertos/semphr.h"
+#include <stdbool.h>
+#include <stdlib.h>
+
+void app_main() {
+    printf("Hello world \n");
+}
