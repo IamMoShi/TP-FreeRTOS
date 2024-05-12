9e726efdd9501ab42d53355bae6475bf7a8f2e94
Leo F <leofornoff02@gmail.com>
2024-05-12 16:40:40 +0200

    sauvegarde, il manque la fin de la connection entre les esp et aussi l'affichage sur l'écran oled

--- TP_FreeRTOS_OLED/src/main.cpp
+++ TP_FreeRTOS_OLED/src/main.cpp
@@ -1,11 +1,294 @@
 #include<Arduino.h>
+#include<stdio.h>
+#include"driver/uart.h"
+#include<Ticker.h>
+#include"DHTesp.h"
 
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println();
+/* //////////////////////////////////////////////////////////////////// */
+/* ----------------------- TEMPERATURE-HUMIDITY ----------------------- */
+/* //////////////////////////////////////////////////////////////////// */
 
+// ───────────────────────────────────────────────────────────────────  //
+void v_HumidityTemperatureTask(void *pvParameters);
+bool b_getTemperature();
+
+DHTesp dht;
+
+TaskHandle_t tempTaskHandle = NULL; // Task handle for the light value read task
+int dhtPin                  = 15;   // Pin number for DHT11 data pin
+
+int temperature; 
+int humidity;    
+
+SemaphoreHandle_t xTemperature;
+SemaphoreHandle_t xHumidity;
+
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_initTemp() {
+  byte resultValue = 0;
+	dht.setup(dhtPin, DHTesp::DHT22);  // Initialize temperature sensor
+	Serial.println("DHT initiated");   // Show that the temperature task was ask
+
+  // ---------------------------------------//
+  xTemperature = xSemaphoreCreateBinary(); 
+  xHumidity    = xSemaphoreCreateBinary();
+
+  xSemaphoreGive(xTemperature);
+  xSemaphoreGive(xHumidity);
+  // ---------------------------------------//
+
+  // Start task to get temperature
+	xTaskCreatePinnedToCore(
+			v_HumidityTemperatureTask,      /* Function to implement the task */
+			"tempTask ",                    /* Name of the task */
+			4000,                           /* Stack size in words */
+			NULL,                           /* Task input parameter */
+			5,                              /* Priority of the task */
+			&tempTaskHandle,                /* Task handle. */
+			1);                             /* Core where the task should run */
 }
 
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_HumidityTemperatureTask(void *pvParameters) {
+  // ---------------------------------------//
+    TickType_t xLastWakeTime;               // Seting up the periodicity of the task
+    const TickType_t xFrequency = 2000;     // Period of the task
+    xLastWakeTime = xTaskGetTickCount();    // The counter
+  // ---------------------------------------//
+
+  while (true) {
+    // ---------------------------------------//
+    xTaskDelayUntil( &xLastWakeTime, xFrequency );
+    xSemaphoreTake(xTemperature, portMAX_DELAY);
+    xSemaphoreTake(xHumidity, portMAX_DELAY);
+    // ---------------------------------------//
+    
+    b_getTemperature();
+
+    // ---------------------------------------//
+    xSemaphoreGive(xTemperature);
+    xSemaphoreGive(xHumidity);
+    // ---------------------------------------//
+
+  }
+}
+
+
+bool b_getTemperature() {
+  /*
+   * To use this function you have to take the mutex(s) of humidity and temperature variables
+   */ 
+
+  TempAndHumidity newValues = dht.getTempAndHumidity();
+
+	if (dht.getStatus() != 0) {
+		//Serial.println("DHT22 error status: " + String(dht.getStatusString()));
+		return false;
+	}
+
+  temperature = newValues.temperature;
+  humidity    = newValues.humidity;
+  return true;
+}
+
+// ───────────────────────────────────────────────────────────────────  //
+
+/* //////////////////////////////////////////////////////////////////////// */
+/* ----------------------- END-TEMPERATURE-HUMIDITY ----------------------- */
+/* //////////////////////////////////////////////////////////////////////// */
+
+/* ////////////////////////////////////////////////////////////////////// */
+/* ----------------------- UART-COMMUNICATION-ESP ----------------------- */
+/* ////////////////////////////////////////////////////////////////////// */
+
+// ───────────────────────────────────────────────────────────────────  //
+void v_uartTask(void *pvParameters);
+void v_initUart();
+
+TaskHandle_t uartTaskHandle = NULL;
+
+int RS232_RX       = 27;
+int RS232_TX       = 26;
+int RX_BUF_SIZE    = 1024;
+
+uint32_t LEN_RS232 = 4;
+int txBytes;
+int rxBytes;
+
+uint8_t DataRX_RS232[4];
+uint8_t data_RS232[4] = {0, 0, 0, 0};
+
+uint16_t detection; 
+int co2; 
+
+SemaphoreHandle_t xDetection; 
+SemaphoreHandle_t xCo2;
+
+const uart_config_t uart_config = { 
+  .baud_rate = 9600, 
+  .data_bits = UART_DATA_8_BITS,
+  .parity = UART_PARITY_DISABLE,
+  .stop_bits = UART_STOP_BITS_1,
+  .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+  .source_clk = UART_SCLK_APB, 
+  };
+
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_initUart() {
+  uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0); 
+  uart_param_config(UART_NUM_1, &uart_config);
+  uart_set_pin(UART_NUM_1, RS232_RX, RS232_TX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+  // ---------------------------------------//
+  xDetection = xSemaphoreCreateBinary();
+  xCo2       = xSemaphoreCreateBinary();
+
+  xSemaphoreGive(xDetection);
+  xSemaphoreGive(xCo2);
+  // ---------------------------------------//
+
+  xTaskCreatePinnedToCore(
+			v_uartTask,                     /* Function to implement the task */
+			"uartTask ",                    /* Name of the task */
+			4000,                           /* Stack size in words */
+			NULL,                           /* Task input parameter */
+			2,                              /* Priority of the task */
+			&uartTaskHandle,                /* Task handle. */
+			0);                             /* Core where the task should run */
+
+
+  Serial.println("UART initiated");
+}
+
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_uartTask(void *pvParameters) {
+  // ---------------------------------------//
+    TickType_t xLastWakeTime;
+    const TickType_t xFrequency = 20;
+    xLastWakeTime = xTaskGetTickCount();
+  // ---------------------------------------//
+
+  while (true) {
+    // ---------------------------------------//
+    xTaskDelayUntil( &xLastWakeTime, xFrequency);
+    xSemaphoreTake(xDetection, portMAX_DELAY);
+    xSemaphoreTake(xCo2, portMAX_DELAY);
+    // ---------------------------------------//
+    rxBytes = uart_read_bytes(UART_NUM_1, DataRX_RS232, LEN_RS232, 1000 / portTICK_PERIOD_MS);
+    if (rxBytes > 0) {
+      co2 = DataRX_RS232[2]*256 + DataRX_RS232[3];
+      detection = ((int)data_RS232[1] << 8) | data_RS232[0];
+    }
+
+    // ---------------------------------------//
+    xSemaphoreGive(xDetection);
+    xSemaphoreGive(xCo2);
+    // ---------------------------------------//
+  }
+}
+
+// ───────────────────────────────────────────────────────────────────  //
+
+/* ////////////////////////////////////////////////////////////////////////// */
+/* ----------------------- END-UART-COMMUNICATION-ESP ----------------------- */
+/* ////////////////////////////////////////////////////////////////////////// */
+
+/* ///////////////////////////////////////////////////////////// */
+/* ----------------------- PRINTING-DATA ----------------------- */
+/* ///////////////////////////////////////////////////////////// */
+
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_initPrinting();
+void v_printingTask(void *pvParameters);
+
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_initPrinting() {
+  // ---------------------------------------//
+  xTemperature = xSemaphoreCreateBinary(); 
+  xHumidity    = xSemaphoreCreateBinary();
+  xDetection   = xSemaphoreCreateBinary();
+  xCo2         = xSemaphoreCreateBinary();
+
+  xSemaphoreGive(xTemperature);
+  xSemaphoreGive(xHumidity);
+  xSemaphoreGive(xDetection);
+  xSemaphoreGive(xCo2);
+  // ---------------------------------------//
+
+  xTaskCreatePinnedToCore(
+			v_printingTask,                 /* Function to implement the task */
+			"printingTask",                 /* Name of the task */
+			4000,                           /* Stack size in words */
+			NULL,                           /* Task input parameter */
+			2,                              /* Priority of the task */
+			&uartTaskHandle,                /* Task handle. */
+			0);                             /* Core where the task should run */
+
+
+  Serial.println("Printing initiated");
+}
+
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_printingTask(void *pvParameters) {
+  // ---------------------------------------//
+    TickType_t xLastWakeTime;
+    const TickType_t xFrequency = 5000;
+    xLastWakeTime = xTaskGetTickCount();
+  // ---------------------------------------//
+
+  while (true) {
+    // ---------------------------------------//
+    xTaskDelayUntil( &xLastWakeTime, xFrequency );
+
+    xSemaphoreTake(xTemperature, portMAX_DELAY);
+    xSemaphoreTake(xHumidity, portMAX_DELAY);
+    xSemaphoreTake(xDetection, portMAX_DELAY);
+    xSemaphoreTake(xCo2, portMAX_DELAY);
+    // ---------------------------------------//
+
+    printf("CO2 : %d; detection : %d; temp : %d; hum : %d\n", co2, detection, temperature, humidity);
+    
+    // ---------------------------------------//
+    xSemaphoreGive(xTemperature);
+    xSemaphoreGive(xHumidity);
+    xSemaphoreGive(xDetection);
+    xSemaphoreGive(xCo2);
+    // ---------------------------------------//
+  }
+}
+
+// ───────────────────────────────────────────────────────────────────  //
+
+/* ///////////////////////////////////////////////////////////////// */
+/* ----------------------- END-PRINTING-DATA ----------------------- */
+/* ///////////////////////////////////////////////////////////////// */
+
+
+// ───────────────────────────────────────────────────────────────────  //
+// ────────────────────────────SETUP──────────────────────────────────  //
+// ───────────────────────────────────────────────────────────────────  //
+void setup() {
+  // --------------------------------------------
+  Serial.begin(115200);
+  Serial.println();
+  // --------------------------------------------
+  v_initUart();
+  // --------------------------------------------
+  v_initTemp();
+  // --------------------------------------------
+  v_initPrinting();
+}
+// ───────────────────────────────────────────────────────────────────  //
+// ───────────────────────────────────────────────────────────────────  //
+// ───────────────────────────────────────────────────────────────────  //
+
 void loop() {
+
 }
--- tp_freeRTOS/src/main.c
+++ tp_freeRTOS/src/main.c
@@ -1,88 +1,129 @@
 #include <stdio.h> 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include "sdkconfig.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "freertos/semphr.h"
-#include <stdbool.h>
-#include <stdlib.h>
-
 #include "ultrasonic.h"
 #include "driver/uart.h"
+#include <string.h>
+#include <math.h>
 
+void affchage_binaire(unsigned int n) {
+    int i;
+    printf("%d en binaire : ", n);
+    for (i = 31; i >= 0; i--) {
+        printf("%d", (n >> i ) & 1);
+    }
+    printf("\n");
+} 
+
+/* ////////////////////////////////////////////////////////////////////////////////////////////// */
 /*---------------------------------------DISTANCE_SENSOR------------------------------------------*/
+/* ////////////////////////////////////////////////////////////////////////////////////////////// */
 
-#define MAX_DISTANCE_CM 500 // 500 cm max
-#define WAKEUP_DISTANCE_CM 10 // Distance de détection de présence
+void v_initDistance();
+void v_getDistance();
+
+// ───────────────────────────────────────────────────────────────────  //
+
+#define MAX_DISTANCE_CM 500              // 500 cm max
 #define WAKEUP_DELAY_SENSIBILITY_MS 5000 // Temps entre la fin de la détection de présence et le signal d'extinction de la présence
 #define TRIGGER_GPIO 33
 #define ECHO_GPIO 32
 
+TaskHandle_t distanceTaskHandle = NULL;
+ultrasonic_sensor_t sensor      = {
+                                    .trigger_pin = TRIGGER_GPIO,
+                                    .echo_pin = ECHO_GPIO
+                                  };
+float distance;
 
-void vget_distance() {
-    ultrasonic_sensor_t sensor = {
-        .trigger_pin = TRIGGER_GPIO,
-        .echo_pin = ECHO_GPIO
-    };
+SemaphoreHandle_t xDistance;
 
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_initDistance() {
     ultrasonic_init(&sensor);
+    
+    // ---------------------------------------//
+    xDistance = xSemaphoreCreateBinary();
+    xSemaphoreGive(xDistance);
+    // ---------------------------------------//
 
+    xTaskCreatePinnedToCore(
+        v_getDistance,
+        "get_distance",
+        configMINIMAL_STACK_SIZE * 3,
+        NULL,
+        5, 
+        &distanceTaskHandle,
+        1
+    );
+    
+    printf("v_initDistance Done\n");
+}
+
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_getDistance() {
     // ---------------------------------------//
     TickType_t xLastWakeTime;
-    const TickType_t xFrequency = 25;
+    const TickType_t xFrequency = 100;
     xLastWakeTime = xTaskGetTickCount();
     // ---------------------------------------//
 
     while (true) {
         // ---------------------------------------//
-        vTaskDelayUntil( &xLastWakeTime, xFrequency );
+        vTaskDelayUntil( &xLastWakeTime, xFrequency);
+        xSemaphoreTake(xDistance, portMAX_DELAY);
         // ---------------------------------------//
 
-        float distance;
-        esp_err_t res = ultrasonic_measure(&sensor, MAX_DISTANCE_CM, &distance);
+        ultrasonic_measure(&sensor, MAX_DISTANCE_CM, &distance);
+        distance = distance * 100;
+        // printf("Distance : %0.04f cm\n", distance);
 
-
-        if (res != ESP_OK) {
-            printf("Error %d: ", res);
-            switch (res)
-            {
-                case ESP_ERR_ULTRASONIC_PING:
-                    printf("Cannot ping (device is in invalid state)\n");
-                    break;
-                case ESP_ERR_ULTRASONIC_PING_TIMEOUT:
-                    printf("Ping timeout (no device found)\n");
-                    break;
-                case ESP_ERR_ULTRASONIC_ECHO_TIMEOUT:
-                    printf("Echo timeout (i.e. distance too big)\n");
-                    break;
-                default:
-                    printf("%s\n", esp_err_to_name(res));
-            }
-        }
-        else {
-            if (distance * 100 < WAKEUP_DISTANCE_CM) {
-                printf("Distance: %0.04f cm\n", distance*100);
-            }       
-        }
+        // ---------------------------------------//
+        xSemaphoreGive(xDistance);
+        // ---------------------------------------//
     }
 }
 
-/*---------------------------------------DISTANCE_SENSOR------------------------------------------*/
+// ───────────────────────────────────────────────────────────────────  //
 
+/* ////////////////////////////////////////////////////////////////////////////////////////////// */
+/*-----------------------------------END_DISTANCE_SENSOR------------------------------------------*/
+/* ////////////////////////////////////////////////////////////////////////////////////////////// */
+
+/* ////////////////////////////////////////////////////////////////////////////////////////////// */
 /*-------------------------------------------CO_SENSOR--------------------------------------------*/
+/* ////////////////////////////////////////////////////////////////////////////////////////////// */
 
+// ───────────────────────────────────────────────────────────────────  //
 
+void v_initCo2();
+void v_get_co2(); 
+
+// ───────────────────────────────────────────────────────────────────  //
 
 int RX_BUF_SIZE = 1024;
-int TXD_PIN = 17;
-int RXD_PIN = 16;
+int TXD_PIN     = 17;
+int RXD_PIN     = 16;
+
 int rxBytes; 
 int txBytes;
+
 uint8_t data[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
 int len = 9;
+
 uint8_t DataRX[9];
-int co2;
+u_int8_t co2_1 = 0;
+u_int8_t co2_2 = 0;
+
+SemaphoreHandle_t xCo2;
 
 const uart_config_t uart_config = {
     .baud_rate = 9600,
@@ -93,10 +134,37 @@ const uart_config_t uart_config = {
     .source_clk = UART_SCLK_APB,
 };
 
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_initCo2() {
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    // ---------------------------------------//
+    xCo2 = xSemaphoreCreateBinary();
+    xSemaphoreGive(xCo2);
+    // ---------------------------------------// 
+    
+    xTaskCreatePinnedToCore(
+        v_get_co2, 
+        "get_co2",
+        configMINIMAL_STACK_SIZE * 3, 
+        NULL,
+        5, 
+        NULL,
+        1
+    );
+
+    printf("v_initCo2 Done \n");
+}
+
+// ───────────────────────────────────────────────────────────────────  //
+
 void v_get_co2() {
     // ---------------------------------------//
     TickType_t xLastWakeTime;
-    const TickType_t xFrequency = 25;
+    const TickType_t xFrequency = 100;
     xLastWakeTime = xTaskGetTickCount();
     // ---------------------------------------//
 
@@ -104,31 +172,127 @@ void v_get_co2() {
 
         // ---------------------------------------//
         vTaskDelayUntil( &xLastWakeTime, xFrequency );
+        xSemaphoreTake(xCo2, portMAX_DELAY);
         // ---------------------------------------//
 
         txBytes = uart_write_bytes(UART_NUM_1, data, len);
         rxBytes = uart_read_bytes(UART_NUM_1, DataRX, len, 1000 / portTICK_PERIOD_MS);
-
+        
         if (rxBytes > 0) {
-            co2 = DataRX[2] * 256 + DataRX[3];
-            printf("%d\n", co2);
+            co2_1 = DataRX[2];
+            co2_2 = DataRX[3];
+            // printf("CO2 : %d\n", co2_1 * 255 + co2_2);
         }
 
-
+        // ---------------------------------------//
+        xSemaphoreGive(xCo2);
+        // ---------------------------------------//
     }
 }
 
-/*-------------------------------------------CO_SENSOR--------------------------------------------*/
+// ───────────────────────────────────────────────────────────────────  //
 
+/* ////////////////////////////////////////////////////////////////////////////////////////////// */
+/*-------------------------------------------END_CO_SENSOR----------------------------------------*/
+/* ////////////////////////////////////////////////////////////////////////////////////////////// */
+
+/* ////////////////////////////////////////////////////////////////////////////////////////////// */
+/*-----------------------------------------UART_CONNECTION_ESP------------------------------------*/
+/* ////////////////////////////////////////////////////////////////////////////////////////////// */
+
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_initUartEsp();
+void v_uartEspTask();
+
+// ───────────────────────────────────────────────────────────────────  //
+
+int RX_BUF_SIZE_2 = 1024;
+int TXD_PIN_2     = 26;
+int RXD_PIN_2     = 27;
+
+int rxBytes_2; 
+int txBytes_2;
+
+uint8_t data_2[4];
+int len_2 = 4;
+
+uint8_t DataRX_2[4];
+
+const uart_config_t uart_config_2 = {
+    .baud_rate = 9600,
+    .data_bits = UART_DATA_8_BITS,
+    .parity = UART_PARITY_DISABLE,
+    .stop_bits = UART_STOP_BITS_1,
+    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    .source_clk = UART_SCLK_APB,
+};
+
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_initUartEsp() {
+    uart_driver_install(UART_NUM_2, RX_BUF_SIZE_2 * 2, 0, 0, NULL, 0); 
+    uart_param_config(UART_NUM_2, &uart_config_2);
+    uart_set_pin(UART_NUM_2, RXD_PIN_2, TXD_PIN_2, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    xTaskCreatePinnedToCore(
+        v_uartEspTask,
+        "uartEspTask",
+        4000,
+        NULL,
+        3,
+        NULL,
+        0
+    );
+
+    printf("v_initUartEsp Done \n");
+}
+
+// ───────────────────────────────────────────────────────────────────  //
+
+void v_uartEspTask() {
+    // ---------------------------------------//
+    TickType_t xLastWakeTime;
+    const TickType_t xFrequency = 100;
+    xLastWakeTime = xTaskGetTickCount();
+    // ---------------------------------------//
+    while (true) {
+        // ---------------------------------------//
+        xTaskDelayUntil( &xLastWakeTime, xFrequency);
+        xSemaphoreTake(xDistance, portMAX_DELAY);
+        xSemaphoreTake(xCo2, portMAX_DELAY);
+        // ---------------------------------------//
+        data_2[0] = (uint8_t)((int) round(distance) & 0xFF); // octet de poids faible
+        data_2[1] = (uint8_t)(((int) round(distance) >> 8) & 0xFF); // octet de poids fort
+
+        printf("Data 0\n");
+        affchage_binaire(data_2[0]);
+        printf("Data 1\n");
+        affchage_binaire(data_2[1]);
+        printf("distance\n");
+        affchage_binaire((int) distance);
+
+        data_2[2] = co2_1;
+        data_2[3] = co2_2;
+
+        txBytes = uart_write_bytes(UART_NUM_2, data_2, len_2);
+        
+        // ---------------------------------------//
+        xSemaphoreGive(xCo2);
+        xSemaphoreGive(xDistance);
+        // ---------------------------------------//
+    }
+}
+
+// ───────────────────────────────────────────────────────────────────  //
+
+/* ////////////////////////////////////////////////////////////////////////////////////////////// */
+/*---------------------------------------END_UART_CONNECTION_ESP----------------------------------*/
+/* ////////////////////////////////////////////////////////////////////////////////////////////// */
 
 void app_main() {
-    xTaskCreate(vget_distance, "get_distance", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
-    
-    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
-    uart_param_config(UART_NUM_1, &uart_config);
-    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-
-    xTaskCreate(v_get_co2, "get_co2", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
-
+    v_initDistance();
+    v_initCo2();
+    v_initUartEsp();
 }
 
